e686fbdab2505c143b3fc3612ca9f7d8b4af6a73
Nicolas Mondain <nicolas@sharingbox.fr>
2023-09-25 16:02:12 +0200

    rm test files

--- docs/assets/js/filters-standard.js
+++ docs/assets/js/filters-standard.js
@@ -1,5 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('load', () => {
 
-	console.log('filters-standard.js', extendedfabric); // eslint-disable-line no-undef
+	const HTMLCanvasElement = document.querySelector('canvas');
+
+	if(HTMLCanvasElement){
+
+		const canvas       = {width: 1200, height: 1800};
+		const shoot        = {width: 1200, height: 1800};
+		const incrustation = {a: 0, x: 100, y: 100, w: 1000, h: 1000};
+		const ratio        = canvas.width / HTMLCanvasElement.offsetWidth;
+
+		const FABRIC = new extendedfabric.Fabric(fabric, 'webgl').extend(); // eslint-disable-line no-undef
+		const CANVAS = new FABRIC.Canvas(HTMLCanvasElement, {backgroundColor: '#eaecef'});
+
+		CANVAS.setDimensions({width: canvas.width / ratio, height: canvas.height / ratio});
+
+	}
 
 });
--- docs/filters-standard.markdown
+++ docs/filters-standard.markdown
@@ -6,10 +6,12 @@ nav_order: 1
 nav_exclude: false
 parent: Filters
 js:
-  - ../../dist/main.js
+  - https://raw.githubusercontent.com/nicolasmondain/fabric/master/dist/main.js
+  - https://cdnjs.cloudflare.com/ajax/libs/fabric.js/5.3.1/fabric.min.js
   - ../assets/js/filters-standard.js
 ---
 
 <h1>Use standard filters</h1>
 <select class="form-select form-select-sm"></select>
+<canvas>
 
